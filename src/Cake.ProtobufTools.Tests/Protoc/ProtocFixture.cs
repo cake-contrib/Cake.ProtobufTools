0368ef87d228112bd1e83047a11145faea78636b
Miha Markič <miha@rthand.com>
2021-03-14 14:49:55 +0100

    Closes #17 References Cake 1.0.0

--- src/Cake.ProtobufTools.Tests/Protoc/ProtocFixture.cs
+++ src/Cake.ProtobufTools.Tests/Protoc/ProtocFixture.cs
@@ -1,21 +1,18 @@
-﻿using Cake.Core;
+﻿using System;
+using Cake.Core;
 using Cake.Core.Configuration;
 using Cake.Core.Diagnostics;
 using Cake.Core.IO;
-using Cake.Core.Tooling;
 using Cake.Testing.Fixtures;
-using NSubstitute;
-using System;
 
 namespace Cake.ProtobufTools.Tests.Protoc
 {
     public class ProtocFixture : ToolFixture<ProtocSettings>, ICakeContext
     {
         public FilePath[] ProtoFiles { get; set; }
-        IFileSystem fileSystem;
-        ICakeEnvironment environment;
-        IFileSystem ICakeContext.FileSystem => fileSystem;
-        ICakeEnvironment ICakeContext.Environment => environment;
+        public string[] Services { get; set; } = new string[0];
+        IFileSystem ICakeContext.FileSystem => FileSystem;
+        ICakeEnvironment ICakeContext.Environment => Environment;
         public ICakeLog Log => Log;
         ICakeArguments ICakeContext.Arguments => throw new NotImplementedException();
         IProcessRunner ICakeContext.ProcessRunner => ProcessRunner;
@@ -24,15 +21,6 @@ namespace Cake.ProtobufTools.Tests.Protoc
         ICakeConfiguration ICakeContext.Configuration => throw new NotImplementedException();
         public ProtocFixture() : base("protoc")
         {
-            Tools = Substitute.For<IToolLocator>();
-            fileSystem = Substitute.For<IFileSystem>();
-            environment = Substitute.For<ICakeEnvironment>();
-            var toolPath = new FilePath("mobile-center");
-            var file = Substitute.For<IFile>();
-            file.Exists.Returns(true);
-            fileSystem.GetFile(toolPath).Returns(file);
-            environment.WorkingDirectory.Returns("C:/Temp");
-            Tools.Resolve("protoc").Returns(toolPath);
             ProcessRunner.Process.SetStandardOutput(new string[] { });
         }
         protected override void RunTool()
