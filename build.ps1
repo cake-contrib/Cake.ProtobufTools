9ca3c3ce00658e993be1516344536894c4b64329
Miha Markič <miha@rthand.com>
2021-03-14 15:12:03 +0100

    Updates build script

--- build.ps1
+++ build.ps1
@@ -5,12 +5,11 @@ $SCRIPT_NAME = "recipe.cake"
 Write-Host "Restoring .NET Core tools"
 dotnet tool restore
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-    [string[]]$ScriptArgs
+
 Write-Host "Bootstrapping Cake"
 dotnet cake $SCRIPT_NAME --bootstrap
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-        return $null
+
 Write-Host "Running Build"
 dotnet cake $SCRIPT_NAME @args
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }Invoke-Expression "& `"$CAKE_EXE`" `"$Script`" -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental $ScriptArgs"
-exit $LASTEXITCODE
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
